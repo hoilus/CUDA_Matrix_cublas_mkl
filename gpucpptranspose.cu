a5fdbcb6531503f2d637bfd1c4adda40346c43fa
Liang Hong <hoilus@users.noreply.github.com>
2018-05-15 11:36:02 -0500

    Update gpucpptranspose.cu

--- gpucpptranspose.cu
+++ gpucpptranspose.cu
@@ -3,43 +3,99 @@
 #include <cstdlib>
 #include <ctime>
 
-const int blockSize = 16;
+const int blockSizex = 32;
+const int blockSizey = 8;
+const int TILE_DIM = blockSizex;
+#define imin(a, b) (a<b?a:b)
 
 // Naive matrix transpose
 __global__
-void gpu_matrix_trans_naive(double *mat_in, double *mat_out, int cols, int rows) {
-  int idx = blockIdx.x * blockDim.x + threadIdx.x;
-  int idy = blockIdx.y * blockDim.y + threadIdx.y;
+void gpu_matrix_trans_naive(double *mat_in, double *mat_out) {
+  int idx = blockIdx.x * TILE_DIM + threadIdx.x;
+  int idy = blockIdx.y * TILE_DIM + threadIdx.y;
+  int width = gridDim.x * TILE_DIM;
 
-  if (idx < cols && idy < rows) {
-    mat_out[idy + idx*rows] = mat_in[idy*cols + idx];
+  for (int j = 0; j < TILE_DIM; j += blockSizey) {
+    mat_out[idy + j + idx*width] = mat_in[(idy+j)*width + idx];
   }
 }
 
 // Transpose via shared memory
 __global__
-void gpu_matrix_trans_sharedmem(double *mat_in, double *mat_out, int cols, int rows) {
+void gpu_matrix_trans_sharedmem(double *mat_in, double *mat_out) {
   // shared memory (48KB/N per block), N is the number of blocks on the same multiprocessor
-  __shared__ double tile[blockSize*blockSize];
+  __shared__ double tile[TILE_DIM*TILE_DIM];
 
-  int idx = blockIdx.x * blockDim.x + threadIdx.x;
-  int idy = blockIdx.y * blockDim.y + threadIdx.y;
+  int idx = blockIdx.x * TILE_DIM + threadIdx.x;
+  int idy = blockIdx.y * TILE_DIM + threadIdx.y;
+  int width = gridDim.x * TILE_DIM;
 
-  if (idx < cols && idy < rows)
-    tile[threadIdx.x + blockSize*threadIdx.y] = mat_in[idy*cols + idx];
+  for (int j = 0; j < TILE_DIM; j += blockSizey) {
+    tile[threadIdx.x + (threadIdx.y+j)*TILE_DIM] = mat_in[(idy+j)*width + idx];
+  }
 
   __syncthreads();
 
-  if (idx < cols && idy < rows)
-    mat_out[idy + idx*rows] = tile[threadIdx.x + blockSize*threadIdx.y];
+
+  for (int j = 0; j < TILE_DIM; j += blockSizey) {
+    mat_out[(idy+j)*width + idx] = tile[threadIdx.x*TILE_DIM + threadIdx.y + j];
+  }
+}
+
+// Coalesced Transpose via shared memory
+__global__
+void gpu_matrix_trans_coales_sharedmem(double *mat_in, double *mat_out) {
+  // shared memory (48KB/N per block), N is the number of blocks on the same multiprocessor
+  __shared__ double tile[TILE_DIM*TILE_DIM];
+
+  int idx = blockIdx.x * TILE_DIM + threadIdx.x;
+  int idy = blockIdx.y * TILE_DIM + threadIdx.y;
+  int width = gridDim.x * TILE_DIM;
+
+  for (int j = 0; j < TILE_DIM; j += blockSizey) {
+    tile[threadIdx.x*TILE_DIM + threadIdx.y + j] = mat_in[(idy+j)*width + idx];
+  }
+
+  __syncthreads();
+
+  idx = blockIdx.y * TILE_DIM + threadIdx.x;
+  idy = blockIdx.x * TILE_DIM + threadIdx.y;
+
+  for (int j = 0; j < TILE_DIM; j += blockSizey) {
+    mat_out[(idy+j)*width + idx] = tile[threadIdx.x + (threadIdx.y+j)*TILE_DIM];
+  }
+}
+
+// Coalesced Transpose via shared memory without bank conflict
+__global__
+void gpu_matrix_trans_coales_sharedmem_NoBankConfl(double *mat_in, double *mat_out) {
+  // shared memory (48KB/N per block), N is the number of blocks on the same multiprocessor
+  __shared__ double tile[TILE_DIM][TILE_DIM+1];
+
+  int idx = blockIdx.x * TILE_DIM + threadIdx.x;
+  int idy = blockIdx.y * TILE_DIM + threadIdx.y;
+  int width = gridDim.x * TILE_DIM;
+
+  for (int j = 0; j < TILE_DIM; j += blockSizey) {
+    tile[threadIdx.y+j][threadIdx.x] = mat_in[(idy+j)*width + idx];
+  }
+
+  __syncthreads();
+
+  idx = blockIdx.y * TILE_DIM + threadIdx.x;
+  idy = blockIdx.x * TILE_DIM + threadIdx.y;
+
+  for (int j = 0; j < TILE_DIM; j += blockSizey) {
+    mat_out[(idy+j)*width + idx] = tile[threadIdx.x][threadIdx.y+j];
+  }
 }
 
 int main(void) {
   int cols = 1<<10, rows = 1<<10;
 
-  int grid_cols = (cols + blockSize - 1)/blockSize;
-  int grid_rows = (rows + blockSize - 1)/blockSize;
-  dim3 dimBlock(blockSize, blockSize, 1);
+  int grid_cols = imin(512, (cols + TILE_DIM - 1)/TILE_DIM);
+  int grid_rows = imin(512, (rows + TILE_DIM - 1)/TILE_DIM);
+  dim3 dimBlock(blockSizex, blockSizey, 1);
   dim3 dimGrid(grid_cols, grid_rows, 1);
 
   // Allocate memory in host RAM
@@ -68,8 +124,10 @@ int main(void) {
   cudaMemcpy(d_mat_in, h_mat_in, cols*rows*sizeof(double), cudaMemcpyHostToDevice);
 
   // Run kernel
-//  gpu_matrix_trans_naive<<<dimGrid, dimBlock>>>(d_mat_in, d_mat_out, cols, rows);
-  gpu_matrix_trans_sharedmem<<<dimGrid, dimBlock>>>(d_mat_in, d_mat_out, cols, rows);
+  gpu_matrix_trans_naive<<<dimGrid, dimBlock>>>(d_mat_in, d_mat_out);
+  gpu_matrix_trans_sharedmem<<<dimGrid, dimBlock>>>(d_mat_in, d_mat_out);
+  gpu_matrix_trans_coales_sharedmem<<<dimGrid, dimBlock>>>(d_mat_in, d_mat_out);
+  gpu_matrix_trans_coales_sharedmem_NoBankConfl<<<dimGrid, dimBlock>>>(d_mat_in, d_mat_out);
 
   // Copy result from device to host
   cudaMemcpy(h_mat_out, d_mat_out, cols*rows*sizeof(double), cudaMemcpyDeviceToHost);
